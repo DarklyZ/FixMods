2048499db9bfa9b12d1a29c3bdcb65421bde5cfb
DarklyZ <daniil.gorbik@gmail.com>
2023-02-26 13:58:06 +0500

    Fixed bewitchment broom in VR

--- src/main/java/darklyz/fixmods/mixin/VRPlayerMixin.java
+++ src/main/java/darklyz/fixmods/mixin/VRPlayerMixin.java
@@ -1,7 +1,7 @@
 package darklyz.fixmods.mixin;
 
+import moriyashiine.bewitchment.api.entity.BroomEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,11 +13,12 @@ import org.vivecraft.gameplay.VRPlayer;
 public class VRPlayerMixin {
 	@Inject(at = @At("HEAD"), method = "doPermanantLookOverride", remap = false, cancellable = true)
 	private void doPermanantLookOverride(ClientPlayerEntity entity, VRData data, CallbackInfo info) {
-		if (entity != null) {
-			Entity vehicle = entity.getVehicle();
-			if (vehicle != null) {
-				entity.sendChatMessage(vehicle.getType().toString(), null);
-			}
+		if (entity != null && entity.getVehicle() instanceof BroomEntity) {
+			entity.setYaw(data.getController(1).getYaw());
+			entity.setHeadYaw(entity.getYaw());
+			entity.setPitch(-data.getController(1).getPitch());
+
+			info.cancel();
 		}
 	}
 }
